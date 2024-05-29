39e6d55dc0075451883adc77f882881426f7121b
Rodqueen <marthaqueen.20@gmail.com>
2024-05-28 21:25:00 -0600

    initial commit

---
+++ js main.js
@@ -0,0 +1,8 @@
+function muestra oculta(id) {
+    let div = document. getElementById(id);
+    if div.style.display "none"){
+div.style,display
+    }
+    else {
+        div.style.display="none";
+}
